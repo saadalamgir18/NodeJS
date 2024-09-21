bd7ddaa2e638ad9c97f6b9a24545ecb2f49a4aa3
saadalamgir18 <saadalamgir18@gmail.com>
2024-09-21 14:56:27 +0500

    refactor code to seprate some functions from getAllTours route, create api for aggrigation, implemented pre middleware to create slug

--- 3-natours/controllers/tourController.js
+++ 3-natours/controllers/tourController.js
@@ -1,9 +1,22 @@
 const Tour = require('./../models/tourModel');
-
+const APIFeatures = require('./../utils/apiFeatures');
 // tour handlers
+
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -84,6 +97,87 @@ exports.deletTour = async (req, res) => {
     res.status(204).json({
       status: 'success',
       data: null,
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.getTourStates = async (req, res) => {
+  try {
+    const states = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        states,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = +req.params.year;
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
+        },
+      },
+      {
+        $addFields: { month: '$_id' },
+      },
+      {
+        $project: { _id: 0 },
+      },
+      {
+        $sort: { numTourStarts: -1 },
+      },
+      {
+        $limit: 12,
+      },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        plan,
+      },
     });
   } catch (err) {
     res.status(400).json({
--- 3-natours/routes/tourRoutes.js
+++ 3-natours/routes/tourRoutes.js
@@ -3,7 +3,12 @@ const tourControler = require('./../controllers/tourController');
 // tour routes
 const router = express.Router();
 // router.param("id", tourControler.checkID);
+router
+  .route('/top-5-cheap')
+  .get(tourControler.aliasTopTours, tourControler.getAllTours);
 
+router.route('/tour-stats').get(tourControler.getTourStates);
+router.route('/monthly-plan/:year').get(tourControler.getMonthlyPlan);
 router.route('/').get(tourControler.getAllTours).post(tourControler.CreateTour);
 router
   .route('/:id')
